1462348966e48d043ac82050894dd1702757ce79
ManiReddy-1808 <manireddy18082002@gmail.com>
2026-02-27 20:54:44 +0530

    Added new code

--- ec2.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_instance" "this" { # Use main / this if only 1 instance
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.sg_ids
-  tags = local.common_tags
-}
